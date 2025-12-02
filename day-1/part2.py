c58c9db4c5f88f13b52f5e4bdfaca321c00aad73
catherine-joyce <cannejoyce@outlook.com>
2025-12-02 01:36:53 +0000

    refactoring

--- day-1/part2.py
+++ day-1/part2.py
@@ -1,36 +1,29 @@
 import re
 from sys import stdin
 
-def create_numbers_list():
-    numbers = []
-    for i in range(0, 100):
-        numbers.append(i)
-    return numbers
+def get_instructions_input():
+    return list(stdin.read().split())
 
-
-def get_new_number_part_2(numbers, current_number, text_input):
+def get_new_number_part_2(current_number, text_input):
     zero_count = 0
     number_string = re.search(r"\d+", text_input)
     number = int(number_string.group())
     if 'L' in text_input:
-        index = (current_number - number) % 100
+        new_number = (current_number - number) % 100
         if current_number != 0 and (current_number - (number % 100)) < 0:
             zero_count = 1
     elif 'R' in text_input:
-        index = (current_number + number) % 100
+        new_number = (current_number + number) % 100
         if current_number != 0 and (current_number + (number % 100)) > 100:
             zero_count = 1
     zero_count += (number // 100)
-    return numbers[index], zero_count
+    return new_number, zero_count
 
-def get_instructions_input():
-    return list(stdin.read().split())
- 
 
-def get_number_of_zeros_part_2(numbers, current_number, list_input):
+def get_number_of_zeros_part_2(current_number, list_input):
     zero_count = 0
     for element in list_input:
-        current_number, zero_number = get_new_number_part_2(numbers, current_number, element)
+        current_number, zero_number = get_new_number_part_2(current_number, element)
         zero_count += zero_number
         if current_number == 0:
             zero_count = zero_count + 1
@@ -40,8 +33,7 @@ def get_number_of_zeros_part_2(numbers, current_number, list_input):
 if __name__ == "__main__":
     list_input = get_instructions_input()
     current_number = 50
-    numbers = create_numbers_list()
-    result = get_number_of_zeros_part_2(numbers, current_number, list_input)
+    result = get_number_of_zeros_part_2(current_number, list_input)
     print(result)
 
 
--- day-1/test_code.py
+++ day-1/test_code.py
@@ -1,6 +1,6 @@
 from part1 import get_new_number, get_number_of_zeros
 
-from part2 import get_number_of_zeros_part_2, get_new_number_part_2, create_numbers_list
+from part2 import get_number_of_zeros_part_2, get_new_number_part_2
 
 def test_get_82_from_L68():
     text_input = 'L68'
@@ -30,70 +30,61 @@ def test_number_of_zeros():
 def test_number_of_zeros_part_2():
     list_input = ['L68', 'L30', 'R48', 'L5', 'R60', 'L55', 'L1', 'L99', 'R14', 'L82']
     current_number = 50
-    numbers = create_numbers_list()
-    result = get_number_of_zeros_part_2(numbers, current_number, list_input)
+    result = get_number_of_zeros_part_2(current_number, list_input)
     assert result == 6
 
 def test_get_82_from_L68_part_2_zero_count():
     text_input = 'L68'
     current_number = 50
-    numbers = create_numbers_list()
-    new_number, zero_count = get_new_number_part_2(numbers, current_number, text_input)
+    new_number, zero_count = get_new_number_part_2(current_number, text_input)
     assert new_number == 82
     assert zero_count == 1
 
 def test_get_0_from_R48_part_2_zero_count():
     text_input = 'R48'
     current_number = 52
-    numbers = create_numbers_list()
-    new_number, zero_count = get_new_number_part_2(numbers, current_number, text_input)
+    new_number, zero_count = get_new_number_part_2(current_number, text_input)
     assert new_number == 0
     assert zero_count == 0
 
 def test_part_2_zero_count_from_R446():
     text_input = 'R446'
     current_number = 50
-    numbers = create_numbers_list()
-    new_number, zero_count = get_new_number_part_2(numbers, current_number, text_input)
+    new_number, zero_count = get_new_number_part_2(current_number, text_input)
     assert new_number == 96
     assert zero_count == 4
 
 def test_part_2_zero_count_from_L665():
     text_input = 'L665'
     current_number = 50
-    numbers = create_numbers_list()
-    new_number, zero_count = get_new_number_part_2(numbers, current_number, text_input)
+    new_number, zero_count = get_new_number_part_2(current_number, text_input)
     assert new_number == 85
     assert zero_count == 7
 
 def test_part_2_zero_count_from_R1000():
     text_input = 'R1000'
     current_number = 50
-    numbers = create_numbers_list()
-    new_number, zero_count = get_new_number_part_2(numbers, current_number, text_input)
+    new_number, zero_count = get_new_number_part_2(current_number, text_input)
     assert new_number == 50
     assert zero_count == 10
 
 def test_part_2_zero_count_from_L250():
     text_input = 'L250'
     current_number = 10
-    numbers = create_numbers_list()
-    new_number, zero_count = get_new_number_part_2(numbers, current_number, text_input)
+    new_number, zero_count = get_new_number_part_2(current_number, text_input)
     assert new_number == 60
     assert zero_count == 3 
 
 def test_part_2_zero_count_from_L250_with_starting_number_50():
     text_input = 'L250'
     current_number = 50
-    numbers = create_numbers_list()
-    new_number, zero_count = get_new_number_part_2(numbers, current_number, text_input)
+    new_number, zero_count = get_new_number_part_2(current_number, text_input)
     assert new_number == 0
     assert zero_count == 2
 
 def test_part_2_zero_count_from_R250_with_starting_number_50():
     text_input = 'R250'
     current_number = 50
-    numbers = create_numbers_list()
-    new_number, zero_count = get_new_number_part_2(numbers, current_number, text_input)
+    new_number, zero_count = get_new_number_part_2(current_number, text_input)
     assert new_number == 0
     assert zero_count == 2
