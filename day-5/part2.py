9a7e3aec459ef92a2c613b73dd632dc444a3fceb
catherine-joyce <cannejoyce@outlook.com>
2025-12-05 09:28:59 +0000

    solved part 2

--- day-5/part2.py
+++ day-5/part2.py
@@ -9,24 +9,58 @@ def get_fresh_ids_range(id_range):
 
 def check_ranges_overlap(range_1, range_2):
     return (
-        (range_1.stop >= range_2.start and range_1.start <= range_2.stop)
-        or (range_1.stop <= range_2.start and range_1.start >= range_2.stop)
+        (range_1.stop > range_2.start and range_1.start < range_2.stop)
+        or (range_1.stop < range_2.start and range_1.start > range_2.stop)
         or (range_1.start >= range_2.start and range_1.stop < range_2.stop)
         or (range_1.start <= range_2.start and range_1.stop > range_2.stop)
     )
 
+def get_number_of_unique_ids(ranges_list):
+    count = 0
+    consolidated_ranges_list = get_final_consolidated_ranges_list(ranges_list)
+    for item in consolidated_ranges_list:
+        count += len(item)
+    return count
+
+def get_final_consolidated_ranges_list(ranges_list):
+    consolidated_ranges_list = get_consolidated_ranges_list(ranges_list)
+    ranges_list = recursive_get_consolidated_ranges_list(consolidated_ranges_list)
+    second_ranges_list = recursive_get_consolidated_ranges_list(ranges_list)
+    return second_ranges_list
+
+    
+
+def recursive_get_consolidated_ranges_list(consolidated_ranges_list):
+    final_consolidated_ranges_list = []
+    for item in consolidated_ranges_list:
+        added = False
+        for i in range(len(final_consolidated_ranges_list)):
+            existing_range = final_consolidated_ranges_list[i]
+            if check_ranges_overlap(item, existing_range) and not added:
+                final_consolidated_ranges_list[i] = range(
+                    min(item.start, existing_range.start),
+                    max(item.stop, existing_range.stop),
+                )
+                added = True
+        if not added:
+            final_consolidated_ranges_list.append(item)
+    return final_consolidated_ranges_list
 
 def get_consolidated_ranges_list(ranges_list):
     consolidated_ranges_list = []
     for item in ranges_list:
         id_range = get_fresh_ids_range(item)
+        added = False
         for i in range(len(consolidated_ranges_list)):
             existing_range = consolidated_ranges_list[i]
-            if check_ranges_overlap(id_range, consolidated_ranges_list[i]):
+            if check_ranges_overlap(id_range, existing_range) and not added:
                 consolidated_ranges_list[i] = range(
                     min(id_range.start, existing_range.start),
                     max(id_range.stop, existing_range.stop),
                 )
+                added = True
+        if not added:
+            consolidated_ranges_list.append(id_range)
     return consolidated_ranges_list
 
 
@@ -44,7 +78,5 @@ def get_numbers_and_ranges_input():
 
 if __name__ == "__main__":
     numbers_list, ranges_list = get_numbers_and_ranges_input()
-    print(numbers_list)
-    print(ranges_list)
-    answer = check_ids_and_get_count(numbers_list, ranges_list)
+    answer = get_number_of_unique_ids(ranges_list)
     print(answer)
--- day-5/test_part2.py
+++ day-5/test_part2.py
@@ -1,11 +1,16 @@
-from part2 import get_consolidated_ranges_list, check_ranges_overlap
+from part2 import get_consolidated_ranges_list, check_ranges_overlap, get_final_consolidated_ranges_list, get_number_of_unique_ids
 
 
-# def test_get_consolidated_ranges_list():
-#     ranges_list = ["3-5", "10-14", "16-20", "12-18"]
-#     consolidated_ranges_list = get_consolidated_ranges_list(ranges_list)
-#     assert consolidated_ranges_list == [range(3, 6), range(10, 21)]
+def test_get_consolidated_ranges_list():
+    ranges_list = ["3-5", "10-14", "16-20", "12-18"]
+    consolidated_ranges_list = get_final_consolidated_ranges_list(ranges_list)
+    assert consolidated_ranges_list == [range(3, 6), range(10, 21)]
 
+def test_get_number_of_unique_ids():
+    count = 0
+    ranges_list = ["3-5", "10-14", "16-20", "12-18"]
+    result = get_number_of_unique_ids(ranges_list)
+    assert result == 14
 
 def test_check_ranges_overlap_true():
     range1 = range(10, 15)
@@ -40,3 +45,41 @@ def test_check_ranges_overlap_true_case_3():
     range2 = range(12, 19)
     result = check_ranges_overlap(range1, range2)
     assert result 
+
+def test_check_ranges_overlap_true_case_4():
+    range1 = range(13, 15)
+    range2 = range(12, 19)
+    result = check_ranges_overlap(range1, range2)
+    assert result 
+
+def test_check_ranges_overlap_true_case_5():
+    range1 = range(1, 20)
+    range2 = range(4, 7)
+    result = check_ranges_overlap(range1, range2)
+    assert result 
+
+
+def test_check_ranges_overlap_true_case_6():
+    range1 = range(1, 9)
+    range2 = range(8, 19)
+    result = check_ranges_overlap(range1, range2)
+    assert result
+
+
+def test_check_ranges_overlap_true_case_7():
+    range1 = range(7, 21)
+    range2 = range(3, 18)
+    result = check_ranges_overlap(range1, range2)
+    assert result 
+
+def test_check_ranges_overlap_false_case_3():
+    range1 = range(1, 4)
+    range2 = range(4, 8)
+    result = check_ranges_overlap(range1, range2)
+    assert not result 
+
+def test_check_ranges_overlap_false_case_4():
+    range1 = range(15, 20)
+    range2 = range(11, 15)
+    result = check_ranges_overlap(range1, range2)
+    assert not result 
